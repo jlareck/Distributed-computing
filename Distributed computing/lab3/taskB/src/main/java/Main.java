5e1922dc3e2a0d31374e82e90d702963573fee6d
jlareck <medik2507@gmail.com>
2021-03-02 00:05:54 +0200

    implemented barbershop execution

---
+++ Distributed computing/lab3/taskB/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Barbershop barbershop = new Barbershop();
+        CustomerGenerator customerGenerator = new CustomerGenerator(barbershop);
+        Barber barber = new Barber(barbershop);
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(barber);
+        executor.execute(customerGenerator);
+
+        stopAfter(executor, Config.Main.workTime);
+    }
+
+
+    private static void stopAfter(ExecutorService executor, int workTime) {
+        try {
+            Thread.sleep(workTime);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        try {
+            executor.shutdownNow();
+            executor.awaitTermination(Config.Main.workTime, TimeUnit.MICROSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
