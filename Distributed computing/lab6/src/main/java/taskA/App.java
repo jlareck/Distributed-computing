c57eb5706a2341e41d67269be29351424a9f3a19
jlareck <medik2507@gmail.com>
2021-03-29 11:29:32 +0300

    implemented lab6

---
+++ Distributed computing/lab6/src/main/java/taskA/App.java
@@ -0,0 +1,10 @@
+package taskA;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Manager manager = new Manager(10);
+        manager.work();
+    }
+}
---
+++ Distributed computing/lab6/src/main/java/taskB/App.java
@@ -0,0 +1,10 @@
+package taskB;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Manager manager = new Manager(10, 70, 70);
+        manager.work();
+    }
+}
